6ad5198e81bf544de61fdf1357dde5bae2f4345f
pjay79 <praveen.jayarajan@gmail.com>
2020-07-30 14:35:38 +1000

    add comments

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ To convert file run the following command:
 
 node app.js './src/files/pharmacy-list.csv'
 
-Make sure you have a .env file in the root with your GOOGLE_API_KEY
+Make sure you have a .env file in the root with your GOOGLE_API_KEY, this needs to be a temporary unrestricted key
 */
 
 require('dotenv').config();
