85f1fb9fe6d52332f17c88ee8bed6f44b1d62072
pjay79 <praveen.jayarajan@gmail.com>
2020-07-31 20:41:05 +1000

    update package to cli

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # CSV Pharmacy Location Parser
 
-This package was created to help convert data from a spreadsheet into an array of objects. 
-
-This data is used specifically for data containing pharmacy names. 
-
-This package will require a valid Google API key with unrestricted access to the Geocoding API. This is used to obtaint the pharmacy coordinates (lat/lng).
+This package was created to help convert data from a spreadsheet into an array of objects.This data is used specifically for data containing pharmacy names. This package will require a valid Google API key with unrestricted access to the Geocoding API. This is used to obtaint the pharmacy coordinates (lat/lng).
 
 Spreadsheet csv file should have the following columns:
 
@@ -15,8 +11,12 @@ Spreadsheet csv file should have the following columns:
 - Post Code
 - Home Delivery Service Available
 
-To convert file run the following command:
+# Installation
 
-```node app.js './src/files/pharmacy-list.csv'```
+```npm install -g generate-pharmacy-list```  
 
-Make sure you have a .env file in the root with your GOOGLE_API_KEY.
+# Useage
+
+```generate-pharmacy-list --filename data.csv --apiKey qwefwer891230923```
+
+
--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-/*
-To convert file run the following command:
-
-node app.js './src/files/pharmacy-list.csv'
-
-Make sure you have a .env file in the root with your GOOGLE_API_KEY, this needs to be a temporary unrestricted key
-*/
+#!/usr/bin/env node
 
 require('dotenv').config();
 const CSVToJSON = require('csvtojson');
 const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
 const axios = require('axios');
 const axiosRetry = require("axios-retry");
 const { Client, Status } = require('@googlemaps/google-maps-services-js');
 
 const client = new Client({});
-
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 axiosRetry(axios, {
   retries: 3,
@@ -26,16 +22,28 @@ axiosRetry(axios, {
   },
 });
 
-(async () => {
+(async function() {
+    clear();
+
+    console.log(chalk.yellow(
+        figlet.textSync('The Pharmacist', { horizontalLayout: 'full' })
+    ));
+
+    console.log(chalk.cyan('Processing...'));                    
+    console.log('--------------------------------------------------------------');
+
     try {
         const args = process.argv.slice(2);
         const filename = args[0];
+        const apiKey = args[1];
         const data = await CSVToJSON().fromFile(filename);
 
+        const currentPath = process.cwd();
+
         const d = new Date();
-        const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
-        const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d)
-        const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+        const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+        const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
+        const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
 
         const currentDate = `${da}-${mo}-${ye}`;
 
@@ -43,35 +51,47 @@ axiosRetry(axios, {
         let id = 0;
 
         for (let item of data) {
-            let newItem = {}
+            let newItem = {};
+
             id++;
+
             newItem.id = id;
+
             newItem.name = item['Pharmacy Name'];
+
             newItem.address = `${item.Address}, ${item.City}, ${item.State} ${item['Post Code']}`;
+
             newItem.suburb = item.City;
+
             newItem.state = item.State;
+
             newItem.postcode = parseInt(item['Post Code']);
+
             newItem.phone = item.Phone;
+
             newItem.homeDelivery = item['Home Delivery Service Available'] === 'Yes' ? true : false;
 
             setTimeout(() => client
                 .geocode({
                     params: {
                         address: newItem.address,
-                        key: GOOGLE_API_KEY,
+                        key: apiKey,
                     },
                     rate: 50,
                     timeout: 2000,
                 })
                 .then((response) => {
-                    newItem.coords = { lat: response.data.results[0].geometry.location.lat, lng: response.data.results[0].geometry.location.lng };
+                    newItem.coords = {
+                        lat: response.data.results[0].geometry.location.lat, lng: response.data.results[0].geometry.location.lng,
+                    };
+                    console.log(newItem);
+                    console.log('--------------------------------------------------------------');
+
                     formattedList.push(newItem);
-                    console.log(
-                        'Generated output:',
-                        formattedList.sort((a,b) => a.id - b.id),
-                    );
+                    formattedList.sort((a,b) => a.id - b.id);
+
                     fs.writeFile(
-                        `./src/data/output-${currentDate}.js`,
+                        `${currentPath}/output-${currentDate}.js`,
                         JSON.stringify(formattedList, null, 4).replace(/"([^"]+)":/g, '$1:'),
                         (err) => {
                         if (err) {  
@@ -83,8 +103,9 @@ axiosRetry(axios, {
                 .catch((error) => {
                     console.log(error);
                 }), 1000 * id);
-        }
+        }   
     } catch (err) {
+        console.log(chalk.red('Error'));
         console.log(err);
     }
 })();
