fd9c35d0ad06d9c8de2d0b52f6fafb97d670a4f7
pjay79 <praveen.jayarajan@gmail.com>
2020-08-01 16:15:32 +1000

    add sample file and output

---
+++ src/data/output-01-Aug-2020.js
@@ -0,0 +1,142 @@
+[
+    {
+        id: 1,
+        name: "Pharmacy 777 Butler",
+        address: "Shop 8 / 2 Captiva Approach, Butler, WA 6036",
+        suburb: "Butler",
+        state: "WA",
+        postcode: 6036,
+        phone: "08 9562 1397",
+        homeDelivery: true,
+        coords: {
+            lat: -31.6529179,
+            lng: 115.7141551
+        }
+    },
+    {
+        id: 2,
+        name: "The Happy Pill Pharmacy",
+        address: "Unit A 29 Railway Tce, Wyalkatchem, WA 6485",
+        suburb: "Wyalkatchem",
+        state: "WA",
+        postcode: 6485,
+        phone: "08 9681 1547",
+        homeDelivery: false,
+        coords: {
+            lat: -31.180909,
+            lng: 117.382382
+        }
+    },
+    {
+        id: 3,
+        name: "Whalan Compounding Pharmacy",
+        address: "Shop 2, 59 Bulolo Drive, Whalan, NSW 2770",
+        suburb: "Whalan",
+        state: "NSW",
+        postcode: 2770,
+        phone: "02 9625 9091",
+        homeDelivery: true,
+        coords: {
+            lat: -33.7543888,
+            lng: 150.8098127
+        }
+    },
+    {
+        id: 4,
+        name: "Marayong Pharmacy",
+        address: "56 Railway Road, Marayong, NSW 2148",
+        suburb: "Marayong",
+        state: "NSW",
+        postcode: 2148,
+        phone: "02 9622 1232",
+        homeDelivery: true,
+        coords: {
+            lat: -33.7466809,
+            lng: 150.8997163
+        }
+    },
+    {
+        id: 5,
+        name: "Middleton Compounding Pharmacy",
+        address: "9 Watts Road, Ryde, NSW 2112",
+        suburb: "Ryde",
+        state: "NSW",
+        postcode: 2112,
+        phone: "02 9878 3030",
+        homeDelivery: true,
+        coords: {
+            lat: -33.793835,
+            lng: 151.102996
+        }
+    },
+    {
+        id: 6,
+        name: "Discount Drug Store Hawker",
+        address: "72 Hawker Place, Hawker, ACT 2614",
+        suburb: "Hawker",
+        state: "ACT",
+        postcode: 2614,
+        phone: "02 6254 4421",
+        homeDelivery: true,
+        coords: {
+            lat: -35.2431655,
+            lng: 149.043745
+        }
+    },
+    {
+        id: 7,
+        name: "Pharmacy 777 Maylands",
+        address: "Shop 3 Maylands Park Shopping Centre, 238 Guildford Road, Maylands, WA 6051",
+        suburb: "Maylands",
+        state: "WA",
+        postcode: 6051,
+        phone: "08 9271 5659",
+        homeDelivery: true,
+        coords: {
+            lat: -31.92992019999999,
+            lng: 115.8963274
+        }
+    },
+    {
+        id: 8,
+        name: "Winthrop Pharmacy",
+        address: "Shop 19 Somerville Boulevard, Shop 19, WA 6150",
+        suburb: "Shop 19",
+        state: "WA",
+        postcode: 6150,
+        phone: "08 9310 5466",
+        homeDelivery: true,
+        coords: {
+            lat: -32.0588808,
+            lng: 115.8317579
+        }
+    },
+    {
+        id: 9,
+        name: "Pharmacy 777 Dog Swamp",
+        address: "Shop 4, Dog Swamp Shopping Centre, 6 Wanneroo Road, Tuart Hill, WA 6060",
+        suburb: "Tuart Hill",
+        state: "WA",
+        postcode: 6060,
+        phone: "08 9444 6325",
+        homeDelivery: true,
+        coords: {
+            lat: -31.9106486,
+            lng: 115.8474688
+        }
+    },
+    {
+        id: 10,
+        name: "Calmans Pharmacy",
+        address: "127 Kennedy Street, Picnic Point, NSW 2213",
+        suburb: "Picnic Point",
+        state: "NSW",
+        postcode: 2213,
+        phone: "02 9773 9464",
+        homeDelivery: true,
+        coords: {
+            lat: -33.9703778,
+            lng: 151.0079538
+        }
+    }
+]
